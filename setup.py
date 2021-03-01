5ae968bde60fe95d20d49d64f8b6bd6d4629dd3d
Stefano Mosconi <stefano.mosconi@gmail.com>
2021-03-01 13:56:31 +0200

    changed the wrong file I guess

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="teachable-school-manager",  # Required
-    version="1.4.1",  # Required
+    version="1.4.3",  # Required
     description="Manage your Teachable school using the unofficial Teachable API",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
